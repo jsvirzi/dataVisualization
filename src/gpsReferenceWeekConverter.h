7c088f1dc07f61c77e3016e97fbc5b22e5c2b123
jsvirzi <jsvirzi@gmail.com>
2017-04-02 20:56:22 -0700

    manually constructed lut

---
+++ src/gpsReferenceWeekConverter.h
@@ -0,0 +1,10 @@
+//
+// Created by jsvirzi on 4/2/17.
+//
+
+#ifndef GPSREFERENCEWEEKCONVERTER_H
+#define GPSREFERENCEWEEKCONVERTER_H
+
+uint64_t secondsAtStartOfReferenceWeek(int week);
+
+#endif //GPSREFERENCEWEEKCONVERTER_H
